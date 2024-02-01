a81e4b67431451aece4c86aea4d0fdd5a1953bc4
AjayJohny-2002 <ajayvjohny2002gmail.com>
2024-02-01 23:51:39 +0530

    Second Com

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import LoginPage from "./pages/loginpage/LoginPage";
-import LandingPage from "./pages/landingPage";
-import {RouterProvider, createBrowserRouter,
-  // Navigate,
-} from "react-router-dom";
+
+import './App.css'
+import { useState , useEffect } from 'react'
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <LandingPage />,
-    },
-    {
-      path: "landing",
-      element: <LandingPage />,
-    },
-    {
-      path: "login",
-      element: <LoginPage />,
-    },
-  
-  ]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(10);
+
+  const [submit, setSubmit] = useState(false);
+  const [userData, setUserData] = useState({
+    name:"" ,
+    lname:"" ,
+    age: 0 ,
+    submit: false
+  });
+
+  useEffect(()=>{
+    console.log("Data")
+    if(userData.age>40){
+      setUserData({...userData, submit:true})
+    }
+  },[userData.age])
   return (
     <>
-      <RouterProvider router={router} />
+      <div className="header">
+    <input onChange = {(e)=> setUserData({...userData, "name": e.target.value} )} type="text" placeholder='Enter name of you'/>
+    <input onChange = {(e)=> setUserData({...userData, "lname": e.target.value} )} type="text" placeholder='Enter last name of you'/>
+
+      <input type="number" onChange = {(e)=>setUserData({...userData, "age": e.target.value} )}    placeholder='Enetr age'/>
+      <button > Submit</button>
+
+      <p>
+        {
+          userData.submit && <>{userData.name} {userData.lname} is {userData.age} years old</>
+        }
+      </p>
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
