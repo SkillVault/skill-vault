d56fb0d744e272f6ff113aa3d413f1369b5b9648
Bibin Jose <bibin.jose@neuflo.io>
2024-02-01 16:18:38 +0530

    sign in page complete

--- src/pages/login page/LoginPage.jsx
+++ src/pages/login page/LoginPage.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "./LoginPage.css";
 function LoginPage() {
-  return <div className="login-container">
-    <div className="container">
-      
+  return (
+    <div className="login-container">
+      <div className="container">
+        <img src="./src/assets/logo.png" />
+        <input type="email" id="userEmail" placeholder="email address"/>
+        <input type="password" id="userPassword" placeholder="password"/>
+        <button className="loginBtn">Login</button>
+        <div className="singleLine">
+          <div className="line"></div>
+          <text>Or</text>
+          <div className="line"></div>
+        </div>
+        <button className="googleLoginBtn">Sign in with google</button>
 
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default LoginPage;
