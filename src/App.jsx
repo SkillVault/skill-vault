cd5ad2e0bf2c5467ba87d88acbbb507e6d9e1756
Jerry2732002 <jerrysebastian2732002@gmail.com>
2024-02-07 15:34:26 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import LoginPage from "./pages/loginpage/LoginPage";
-import LandingPage from "./pages/landingPage";
-import {RouterProvider, createBrowserRouter,
+import LandingPage from "./pages/landingpage/LandingPage";
+import Homepage from "./pages/homepage/Homepage";
+
+import {
+  RouterProvider,
+  createBrowserRouter,
   // Navigate,
 } from "react-router-dom";
 
 function App() {
   const router = createBrowserRouter([
+    {
+      path: "/", // Define a route for the root path
+      element: <LandingPage />, // Render LandingPage at the root
+    },
     {
       path: "landing",
       element: <LandingPage />,
@@ -16,9 +24,10 @@ function App() {
     },
   ]);
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    // <>
+    //   <RouterProvider router={router} />
+    // </>
+    <Homepage />
   );
 }
 
--- src/pages/loginpage/LoginPage.jsx
+++ src/pages/loginpage/LoginPage.jsx
@@ -1,26 +1,32 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
+import LandingNavbar from "../../components/LandingNav/LandingNav";
 
 import "./LoginPage.css";
 function LoginPage() {
   const navigate = useNavigate();
   return (
-    <div className="login-container">
-      <div className="container">
-        <img src="./src/assets/logo.png" />
-        <input type="email" id="userEmail" placeholder="email address"/>
-        <input type="password" id="userPassword" placeholder="password"/>
-        <button className="loginBtn">Login</button>
-        <div className="singleLine">
-          <div className="line"></div>
-          <text>Or</text>
-          <div className="line"></div>
-        </div>
-        
-        <button className="googleLoginBtn" onClick={()=>navigate('/landing')}>Sign in with google</button>
-      
+    <div>
+      <LandingNavbar />
+      <div className="login-container">
+        <div className="container">
+          <img src="./src/assets/logo.png" />
+          <input type="email" id="userEmail" placeholder="email address" />
+          <input type="password" id="userPassword" placeholder="password" />
+          <button className="loginBtn">Login</button>
+          <div className="singleLine">
+            <div className="line"></div>
+            <text>Or</text>
+            <div className="line"></div>
+          </div>
 
+          <button
+            className="googleLoginBtn"
+            onClick={() => navigate("/landing")}
+          >
+            Sign in with google
+          </button>
+        </div>
       </div>
     </div>
   );
