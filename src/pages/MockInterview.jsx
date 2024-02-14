417777d5ce73d19f156822587dc1b54ffafe2ea2
Bibin Jose <bibin.jose@neuflo.io>
2024-02-14 16:31:42 +0530

    added camera to webisite

--- src/pages/MockInterview.jsx
+++ src/pages/MockInterview.jsx
@@ -1,22 +1,36 @@
 import React from "react";
 import "./MockInterview.css";
+import Webcam from "react-webcam";
 
 const MockInterview = () => {
+  const videoConstraints = {
+    width: 400,
+    height: 300,
+    facingMode: "user",
+  };
   return (
     <div className="mock-screen">
       <div className="mock-container">
         <h1>1.What is React ?</h1>
-        <div className="mock">
-        <img src="./src/assets/timer.png" alt="timer" id="timer"/>
-        <div className="audio-section">
-          <h2>01:20</h2>
+        <div className="mock-main">
+          <div className="mock">
+            <img src="./src/assets/timer.png" alt="timer" id="timer" />
+            <h2>01:20</h2>
+          </div>
           <div className="audio">
             <img src="./src/assets/mic.png" alt="" />
             <img src="./src/assets/stop.png" alt="" />
           </div>
-          </div>
         </div>
-        <img src="./src/assets/video.png" alt="video player" id="video-player" />
+
+        <Webcam
+          audio={false}
+          height={300}
+          id="video-player"
+          screenshotFormat="image/jpeg"
+          width={400}
+          videoConstraints={videoConstraints}
+        />
       </div>
     </div>
   );
