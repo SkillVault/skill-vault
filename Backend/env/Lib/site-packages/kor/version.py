2d4c0a47e944443141d7c924121d71be2bb74996
BIBS23 <bibinoottukulathil@gmail.com>
2024-04-18 09:00:51 +0530

    fixed all error

--- Backend/env/Lib/site-packages/kor/version.py
+++
@@ -1,7 +0,0 @@
-"""Get the version of the package."""
-from importlib import metadata
-
-try:
-    __version__ = metadata.version("kor")
-except metadata.PackageNotFoundError:
-    __version__ = "local"
